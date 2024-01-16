6999bccca91b067f1973dadf7e391e3ab98ee600
Matiyas1994 <matiyasy1@gmail.com>
2024-01-16 14:01:06 +0300

    "TG_Bot"

---
+++ bot.py
@@ -0,0 +1,125 @@
+import os
+import logging
+import texts
+from telegram import Update , InlineKeyboardButton, InlineKeyboardMarkup , constants
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler
+
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = texts.text1
+    keyboard = [
+        [[ InlineKeyboardButton("How Do I Join A2SV Community Education?", callback_data="2")]],
+        [[ InlineKeyboardButton("How Do I Join A2SV?", callback_data="1", url="https://t.me//CommunityTrial_bot?start")]]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard[0])
+
+    
+    # await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('assets/a2sv.jpg', 'rb'))
+    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, 
+                                   reply_markup=reply_markup, parse_mode = constants.ParseMode.HTML)
+
+    
+
+
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer() 
+    text1 = texts.text1
+    text2 = texts.text2
+    text3 = texts.text3
+    text4 = texts.text4
+    text5 = texts.text5
+    faq = texts.faq
+    announcement = texts.announcements
+    contest = texts.contest
+    discussion = texts.discussion
+
+    keyboard = [
+       [ [ InlineKeyboardButton("How Do I Get Started?", callback_data="2")] ],
+       [ [ InlineKeyboardButton("Step 2", callback_data="3")] ],
+       [ [ InlineKeyboardButton("Step 3", callback_data="4")] ],
+       [ [ InlineKeyboardButton("Guide", callback_data="5")] ],
+    ]
+    
+    keyboard2 = [[ InlineKeyboardButton("FAQs", callback_data="faq"), InlineKeyboardButton("Announcements", callback_data="announcement")],
+                 [ InlineKeyboardButton("Contests", callback_data="contest"), InlineKeyboardButton("Discussion", callback_data="discussion")],
+                 [ InlineKeyboardButton("Go Back To The Group", url= "https://t.me/A2SVDiscussion", callback_data="go_back_to_the_group")]]
+    
+    reply_markup1 = InlineKeyboardMarkup(keyboard[0])
+    reply_markup2 = InlineKeyboardMarkup(keyboard[1])
+    reply_markup3 = InlineKeyboardMarkup(keyboard[2])
+    reply_markup4 = InlineKeyboardButton(keyboard[3])
+    reply_markup5 = InlineKeyboardMarkup(keyboard2)
+    # reply_markup6 = InlineKeyboardButton(keyboard[4])
+
+    if query.data == "1":
+        pass
+        # await context.bot.send_message(chat_id=query.message.chat_id, text=text1, reply_markup=reply_markup1, parse_mode= constants.ParseMode.HTML)
+    elif query.data =="2":
+        await context.bot.sendMessage(chat_id=query.message.chat_id, text=text2, reply_markup=reply_markup2, parse_mode= constants.ParseMode.HTML)
+    elif query.data =="3":
+         await context.bot.sendMessage(chat_id=query.message.chat_id, text=text3, reply_markup=reply_markup3, parse_mode= constants.ParseMode.HTML)
+    elif query.data =="4":
+         await context.bot.sendMessage(chat_id=query.message.chat_id, text=text4, reply_markup=reply_markup5, parse_mode= constants.ParseMode.HTML)
+    elif query.data =="5":
+         await context.bot.sendMessage(chat_id=query.message.chat_id, text=text5, reply_markup=reply_markup5, parse_mode= constants.ParseMode.HTML)
+    elif query.data == "faq":
+        await context.bot.sendMessage(chat_id=query.message.chat_id, text=faq, parse_mode= constants.ParseMode.HTML)
+
+    elif query.data == "announcement":
+        await context.bot.sendMessage(chat_id=query.message.chat_id, text=announcement,  parse_mode= constants.ParseMode.HTML)
+  
+    elif query.data == "contest":
+        await context.bot.sendMessage(chat_id=query.message.chat_id, text=contest,  parse_mode= constants.ParseMode.HTML)
+  
+    elif query.data == "discussion":
+        await context.bot.sendMessage(chat_id=query.message.chat_id, text=discussion, parse_mode= constants.ParseMode.HTML)
+    
+    
+
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('6607983770:AAH0ZPcVGzsw_Pk56ElVUcja4dVgtju6aXI').build()
+    
+
+    TOKEN = "6607983770:AAH0ZPcVGzsw_Pk56ElVUcja4dVgtju6aXI"
+    PORT = int(os.environ.get('PORT', '8443'))
+    # add handlers
+    
+    start_handler = CommandHandler('start', start)
+    button_handler = CallbackQueryHandler(button)
+
+    application.add_handler(start_handler)
+    application.add_handler(button_handler)
+
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        secret_token='ASecretTokenIHaveChangedByNow',
+        key='private.key',
+        cert='cert.pem',
+        webhook_url="https://<appname>.herokuapp.com/"
+    )
+    
+    # application.run_polling(allowed_updates=Update.ALL_TYPES)
+    
+#     application.run_webhook(
+#     listen='0.0.0.0',
+#     port=8443,
+#     secret_token='6607983770:AAH0ZPcVGzsw_Pk56ElVUcja4dVgtju6aXI',
+#     
+#     webhook_url='https://a2svcommunitybot:8443'
+# )
+    
+    
+
+   
---
+++ texts.py
@@ -0,0 +1,50 @@
+text = """
+    🚀<b> Introducing A2SV: Empowering University Students to Digitally Transform Africa! 🌍 </b>
+
+🌍 A2SV is an academy that enables high-potential university students to create digital solutions to Africa’s most pressing problems. The program is free for students, making the opportunity available for youth who have talent but lack the means to use it. A2SV upskills high-potential university students, connects them with opportunities at top tech companies around the world. 
+
+A2SV’s intense training program prepares students for internships and full-time jobs with companies like Google, Palantir, Amazon, LinkedIn and Bloomberg. These opportunities give students the experience they need to design and build products that improve the quality of life in Africa. 💡🚀 Students who passed their interviews get internships to gain experience in building scalable products that are widely used around the world. 🌟💻
+    """
+text1 = """
+    🚀 <b>Embark on Your Coding Journey with A2SV </b>
+
+We provide free, comprehensive In-person and Remote Education programs designed to prepare you for a successful career in tech. Whether you're a beginner or an experienced coder, A2SV has the perfect program for you.
+
+Our Community Education Program is your gateway to A2SV's In-person and Remote Education Programs. This introductory program is open to all, regardless of your current education level or programming experience.
+
+    """
+text2 = """
+        <b> Step 1: Apply to the Community Education Program. 📋</b>
+
+Begin your journey by completing our Community Education <a href="https://docs.google.com/forms/d/e/1FAIpQLSfW6XLx9-olkKnmXkOZbYjBwo-R398nPaNyoV8qBwwkfmUJQw/viewform">Application Form</a>, a quick and easy process that will take just 1-3 minutes ⏰. We will automatically send you a tailored learning path.🎯
+    """
+text3 = """
+    <b> Step 2: Complete your tailored learning path and  Participate in two A2SV community contests </b> held every Saturday. Contest links are in the group's <a href="https://t.me/c/2088770460/10"> contests topic.</a>
+    """
+text4 = """
+    <b>Step 3: Join the Waitlist to enroll in our community education classes.📚.</b>
+
+Complete our <a href="https://docs.google.com/forms/d/e/1FAIpQLSfmWXk54sk1L2sYwt33dtgZhYlpKPY4u2Y1AvP87vQJcQgszQ/viewform"> Waitlist Form </a> to express your interest in our upcoming Data Structures and Algorithms course. Once we have sufficient enrollment, we will assign you to one of our community education classes. Our dedicated community coordinators will guide you throughout this educational journey, ensuring you reach your full potential.
+
+Take the first step today and unlock your potential as a skilled software engineer!
+
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSfW6XLx9-olkKnmXkOZbYjBwo-R398nPaNyoV8qBwwkfmUJQw/viewform"> Click here </a>  to apply to the Community Education Program now!🚀
+    """
+text5 = """
+This carefully structured group is designed to provide you with a comprehensive understanding of A2SV, our programs, and the opportunities we offer. Additionally, it serves as a platform for open discussions, knowledge sharing, and collaborative growth.
+
+Guide  for Your Exploration:
+"""
+faq = """
+❓<a href="https://t.me/c/2088770460/14">FAQ Topic </a> \n
+ Check out our Frequently Asked Questions (FAQ) section for straightforward answers to common questions about A2SV. This helpful resource covers a wide range of topics, so you might find the answers you're looking for. We highly recommend checking out this section to get a clear understanding of our programs, opportunities, and overall approach.
+"""
+announcements = """
+📢 <a href="https://t.me/c/2088770460/12"> Announcements Topic </a> \n Stay informed and connected through our Announcements Topic, where we share important updates, milestones, and exciting opportunities. Keep an eye out for this section to stay up-to-date of the latest developments at A2SV.
+"""
+contest = """
+🔥<a href="https://t.me/c/2088770460/10"> Contests Topic </a> \n Put your programming skills to the test by participating in our regular Saturday community contests. Links to these contests are shared in the Contests Topic, providing you with a chance to challenge yourself and engage with fellow contestants.
+"""
+discussion = """
+#️⃣<a href="https://t.me/c/2088770460/1" > Discussion Topic </a> \n Engage in open discussions and share your valuable insights in our Discussion Topic. This interactive space allows you to exchange ideas, seek feedback, and connect with other members of the A2SV community. We encourage you to express your thoughts freely while adhering to our Code of Conduct and respecting personal boundaries.
+"""
