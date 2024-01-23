f129645d44f6059a85471efaa0f3f72ed92de2c8
Matiyas1994 <matiyasy1@gmail.com>
2024-01-23 19:35:31 +0300

    improvement of the content

--- texts.py
+++ texts.py
@@ -19,12 +19,15 @@ text2 = """
 Begin your journey by completing our Community Education <a href="https://docs.google.com/forms/d/e/1FAIpQLSfW6XLx9-olkKnmXkOZbYjBwo-R398nPaNyoV8qBwwkfmUJQw/viewform">Application Form</a>, a quick and easy process that will take just 1-3 minutes ⏰. We will automatically send you a tailored learning path.🎯
     """
 text3 = """
-    <b> Step 2: Complete your tailored learning path and  Participate in two A2SV community contests </b> held every Saturday. Contest links are in the group's <a href="https://t.me/c/2088770460/10"> contests topic.</a>
+    <b> Step 2: Complete your tailored learning path and participate in two consecutive A2SV community contests </b> held every Saturday.\n
+      A2SV Community Contests are programming competitions where you'll race against fellow contestants to solve as many problems as you possible can within a given timeframe. These contests are held remotely every Saturday from 6:00 PM to 8:00 PM.
+
+To participate in these contests, you'll need to register on the <a href= "https://codeforces.com/"> Codeforces </a> platform and register for the upcoming contest using the provided link. Contest links are regularly shared in the group's <a href="https://t.me/A2SVDiscussion/10041"> contests topic.</a>
     """
 text4 = """
     <b>Step 3: Join the Waitlist to enroll in our community education classes.📚.</b>
 
-Complete our <a href="https://docs.google.com/forms/d/e/1FAIpQLSfmWXk54sk1L2sYwt33dtgZhYlpKPY4u2Y1AvP87vQJcQgszQ/viewform"> Waitlist Form </a> to express your interest in our upcoming Data Structures and Algorithms course. Once we have sufficient enrollment, we will assign you to one of our community education classes. Our dedicated community coordinators will guide you throughout this educational journey, ensuring you reach your full potential.
+Complete our <a href="https://forms.gle/8S8yZ784gDy7TvyA9"> Waitlist Form </a> to express your interest in our upcoming Data Structures and Algorithms course. Once we have sufficient enrollment, we will assign you to one of our community education classes. Our dedicated community coordinators will guide you throughout this educational journey, ensuring you reach your full potential.
 
 Take the first step today and unlock your potential as a skilled software engineer!
 
@@ -36,15 +39,18 @@ This carefully structured group is designed to provide you with a comprehensive 
 Guide  for Your Exploration:
 """
 faq = """
-❓<a href="https://t.me/c/2088770460/14">FAQ Topic </a> \n
+❓<a href="https://t.me/A2SVDiscussion/5964">FAQ Topic </a> \n
  Check out our Frequently Asked Questions (FAQ) section for straightforward answers to common questions about A2SV. This helpful resource covers a wide range of topics, so you might find the answers you're looking for. We highly recommend checking out this section to get a clear understanding of our programs, opportunities, and overall approach.
 """
 announcements = """
-📢 <a href="https://t.me/c/2088770460/12"> Announcements Topic </a> \n Stay informed and connected through our Announcements Topic, where we share important updates, milestones, and exciting opportunities. Keep an eye out for this section to stay up-to-date of the latest developments at A2SV.
+📢 <a href="https://t.me/A2SVDiscussion/5954"> Announcements Topic </a> \n 
+ Stay informed and connected through our Announcements Topic, where we share important updates, milestones, and exciting opportunities. Keep an eye out for this section to stay up-to-date of the latest developments at A2SV.
 """
 contest = """
-🔥<a href="https://t.me/c/2088770460/10"> Contests Topic </a> \n Put your programming skills to the test by participating in our regular Saturday community contests. Links to these contests are shared in the Contests Topic, providing you with a chance to challenge yourself and engage with fellow contestants.
+🔥<a href="https://t.me/A2SVDiscussion/10041"> Contests Topic </a> \n
+ Put your programming skills to the test by participating in our regular Saturday community contests. Links to these contests are shared in the Contests Topic, providing you with a chance to challenge yourself and engage with fellow contestants.
 """
 discussion = """
-#️⃣<a href="https://t.me/c/2088770460/1" > Discussion Topic </a> \n Engage in open discussions and share your valuable insights in our Discussion Topic. This interactive space allows you to exchange ideas, seek feedback, and connect with other members of the A2SV community. We encourage you to express your thoughts freely while adhering to our Code of Conduct and respecting personal boundaries.
+#️⃣<a href="https://t.me/A2SVDiscussion/1" > Discussion Topic </a> \n 
+ Engage in open discussions and share your valuable insights in our Discussion Topic. This interactive space allows you to exchange ideas, seek feedback, and connect with other members of the A2SV community. We encourage you to express your thoughts freely while respecting personal boundaries of others.
 """
